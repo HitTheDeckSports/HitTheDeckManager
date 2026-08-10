2e06d9b66d43a616980ba48d3f697035a878ba30
ec1066 <eric.conn23@gmail.com>
2026-08-10 01:40:42 -0500

    Set up Firebase project foundation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -12,9 +13,12 @@ import 'features/transactions/data/repositories/in_memory_deal_repository.dart';
 import 'features/transactions/data/repositories/in_memory_transaction_repository.dart';
 import 'features/transactions/presentation/providers/deal_providers.dart';
 import 'features/transactions/presentation/providers/transaction_providers.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   final contactRepository = InMemoryContactRepository(
     initialContacts: DevelopmentConfig.useSampleData
