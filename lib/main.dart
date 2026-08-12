84f9325e86f07a242810b4ecf4b53ede4b0f7d2b
ec1066 <eric.conn23@gmail.com>
2026-08-11 22:27:44 -0500

    Connect transactions to Firestore

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app/app.dart';
-import 'core/config/development_config.dart';
-import 'core/config/development_sample_data.dart';
 import 'features/transactions/data/repositories/in_memory_deal_repository.dart';
-import 'features/transactions/data/repositories/in_memory_transaction_repository.dart';
 import 'features/transactions/presentation/providers/deal_providers.dart';
-import 'features/transactions/presentation/providers/transaction_providers.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -16,30 +12,11 @@ Future<void> main() async {
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  final dealRepository = InMemoryDealRepository(
-    initialDeals: DevelopmentConfig.useSampleData
-        ? DevelopmentSampleData.createDeals()
-        : const [],
-  );
-
-  final transactionRepository = InMemoryTransactionRepository(
-    initialSales: DevelopmentConfig.useSampleData
-        ? DevelopmentSampleData.createSales()
-        : const [],
-    initialRepairs: DevelopmentConfig.useSampleData
-        ? DevelopmentSampleData.createRepairs()
-        : const [],
-    initialTrades: DevelopmentConfig.useSampleData
-        ? DevelopmentSampleData.createTrades()
-        : const [],
-  );
+  final dealRepository = InMemoryDealRepository(initialDeals: const []);
 
   runApp(
     ProviderScope(
-      overrides: [
-        transactionRepositoryProvider.overrideWithValue(transactionRepository),
-        dealRepositoryProvider.overrideWithValue(dealRepository),
-      ],
+      overrides: [dealRepositoryProvider.overrideWithValue(dealRepository)],
       child: const HitTheDeckApp(),
     ),
   );
